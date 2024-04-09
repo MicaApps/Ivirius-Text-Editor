6bba8d20f0c53a4edbbf7cbc859aed6c4b6266a1
jpb <jakub.baranski.33@gmail.com>
2024-04-09 08:43:33 +0100

    Update version number in AssemblyInfo

--- Ivirius Text Editor/Properties/AssemblyInfo.cs
+++ Ivirius Text Editor/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Ivirius")]
 [assembly: AssemblyProduct("Ivirius Text Editor")]
-[assembly: AssemblyCopyright("Copyright ©  2021-2023")]
+[assembly: AssemblyCopyright("Copyright ©  2021-2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyVersion("3.0.1.0")]
+[assembly: AssemblyFileVersion("3.0.1.0")]
 [assembly: ComVisible(false)]
