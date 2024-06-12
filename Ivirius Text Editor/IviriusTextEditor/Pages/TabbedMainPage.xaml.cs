becf71ac7f40b20e675d3d2a4360ed622add6a1a
jpb <jakub.baranski.33@gmail.com>
2024-06-12 08:33:42 +0100

    Add links to Rebound News

--- Ivirius Text Editor/IviriusTextEditor/Pages/TabbedMainPage.xaml.cs
+++ Ivirius Text Editor/IviriusTextEditor/Pages/TabbedMainPage.xaml.cs
@@ -498,7 +498,6 @@ namespace IviriusTextEditor.Pages
                 if ((string)LocalSettings.Values["News"] == "On")
                 {
                     NewsItem.Visibility = Visibility.Visible;
-                    StoreItem.Visibility = Visibility.Visible;
                     WebsiteItem.Visibility = Visibility.Visible;
                     BugItem.Visibility = Visibility.Visible;
                     YoutubeItem.Visibility = Visibility.Visible;
@@ -506,7 +505,6 @@ namespace IviriusTextEditor.Pages
                 if ((string)LocalSettings.Values["News"] == "Off")
                 {
                     NewsItem.Visibility = Visibility.Collapsed;
-                    StoreItem.Visibility = Visibility.Collapsed;
                     WebsiteItem.Visibility = Visibility.Collapsed;
                     BugItem.Visibility = Visibility.Collapsed;
                     YoutubeItem.Visibility = Visibility.Collapsed;
@@ -516,7 +514,6 @@ namespace IviriusTextEditor.Pages
             {
                 LocalSettings.Values["News"] = "On";
                 NewsItem.Visibility = Visibility.Visible;
-                StoreItem.Visibility = Visibility.Visible;
                 WebsiteItem.Visibility = Visibility.Visible;
                 BugItem.Visibility = Visibility.Visible;
                 YoutubeItem.Visibility = Visibility.Visible;
@@ -3493,7 +3490,7 @@ namespace IviriusTextEditor.Pages
 
         private async void MenuFlyoutItem_Click_7(object Sender, RoutedEventArgs EvArgs)
         {
-            _ = await Launcher.LaunchUriAsync(new Uri("https://errortek-news.vercel.app"));
+            _ = await Launcher.LaunchUriAsync(new Uri("https://rebound-news-fluent.vercel.app"));
         }
 
         private async void MenuFlyoutItem_Click_8(object Sender, RoutedEventArgs EvArgs)
